a614e17bf6b5338960d36544d99680dae8e3aa38
tomek <masterworks41@gmail.com>
2015-07-27 11:36:42 +0200

    init push

---
+++ app/src/main/java/com/tomek/ujebse/Links.java
@@ -0,0 +1,11 @@
+package com.tomek.ujebse;
+
+import com.orm.SugarRecord;
+
+/**
+ * Created by tomek on 25.07.15.
+ */
+public class Links extends SugarRecord <Links> {
+    String original;
+    String shortcut;
+}
---
+++ app/src/main/java/com/tomek/ujebse/ShortcuttingFragment.java
@@ -0,0 +1,27 @@
+package com.tomek.ujebse;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ShortcuttingFragment extends Fragment {
+
+
+    public ShortcuttingFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_shortcutting, container, false);
+    }
+
+
+}
